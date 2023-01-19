553f63e33593aed1d6d1c57e6227548b16fbdcbe
Kaspervdlaan <kaspervdlaan@gmail.com>
2023-01-19 02:53:57 +0100

    Setting up database, styling done for now

---
+++ api/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const app = express();
+
+const salt = bcrypt.genSaltSync(10);
+const hash = bcrypt.hashSync("B4c0/\/", salt);
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://Kappie:Boekie@cluster0.atud80z.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register',async(req,res) => {
+    const {username,password} = req.body;
+    try {
+        const userDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password,salt),
+        })
+        res.json(userDoc);
+    } catch (e) {
+        res.status(400).json(e);
+    }
+});
+
+app.post('/login',async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    res.json(passOk);
+})
+
+app.listen(4000);
+
+//mongodb+srv://Kappie:Boekie@cluster0.atud80z.mongodb.net/?retryWrites=true&w=majority
